d041a065c3d47304617f005bab631e23ef120741
anechaev <anechaev@plesk.com>
2019-10-31 23:42:26 +0700

    kubernetes-2

---
+++ kubernetes/terraform/cluster.tf
@@ -0,0 +1,43 @@
+resource "google_container_cluster" "reddit_cluster" {
+  name     = "reddit-cluster"
+  location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  addons_config {
+    network_policy_config {
+      disabled = true
+    }
+  }
+
+  master_auth {
+    username = "${var.username}"
+    password = "${var.password}"
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = "${google_container_cluster.reddit_cluster.name}"
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
